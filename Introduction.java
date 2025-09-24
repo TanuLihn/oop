15496edcdc1d85c691c8d26bf8fc3912eb63cdb0
TanuLihn <24020202@vnu.edu.vn>
2025-09-24 08:18:27 +0700

    add Introduction

---
+++ Introduction.java
@@ -0,0 +1,12 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Nguyễn Tuấn Linh" + '\t' + "24020202" + '\t' + "IT4" + '\t' + "https://github.com/TanuLihn/oop" + '\t' + "24020202@vnu.edu.vn");
+        for (int i = 9; i > 1; i--){
+            System.out.println(i + " bottles of beer on the wall," + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around, ");
+        }
+        System.out.println(1 + " bottles of beer on the wall," + 1 + " bottles of beer.");
+        System.out.println("Take one down, pass it around, ");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
